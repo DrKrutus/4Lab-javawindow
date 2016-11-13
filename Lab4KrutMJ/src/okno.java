51f731dea0def51dac8a2581fd1ea34e6e990638
DrKrutus <thekrutus@gmail.com>
2016-11-13 13:49:13 +0200

    4 Labwork

--- Lab4KrutMJ/src/okno.java
+++ Lab4KrutMJ/src/okno.java
@@ -1,27 +1,20 @@
 /**
  * Created by Maxim on 13.11.2016.
  */
-
-import javax.swing.JButton;
+        import javax.swing.JButton;
         import javax.swing.JFrame;
         import javax.swing.JLabel;
         import javax.swing.JTextField;
         import java.awt.event.ActionEvent;
         import java.awt.event.ActionListener;
-
-
 public class okno extends JFrame {
     private int count = 0;
-
     private JTextField fio = new JTextField(20);
     private JLabel metka = new JLabel("Ваше имя?");
     private JLabel metkaDva = new JLabel("");
-    private JLabel metkatri = new JLabel("Нажато"+count+"раз");
-
     class PushingListener implements ActionListener {
         public void actionPerformed(ActionEvent e){
             Object button = e.getSource();
-
             if(button instanceof JButton){
                 count++;
                 ((JButton)button).setText("Нажато " + count + " раз");
@@ -29,31 +22,36 @@ public class okno extends JFrame {
             }
         }
     }
-
+    class WinResize implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            Object button1 = e.getSource();
+            if (button1 instanceof JButton) {
+                okno.super.setSize(500, 500);
+            }
+        }
+    }
     public okno(String title)
     {
         super(title);
         SozdatxOkno();
     }
-
     private void SozdatxOkno(){
-
         JButton button = new JButton(" Нажми меня! ");
         button.setBounds(65,60,150,40);
-
         button.addActionListener(new PushingListener());
-
+        JButton button1 = new JButton(" Бусткнопка ");
+        button1.setBounds(65,120,150,40);
+        button1.addActionListener(new WinResize());
         metka.setBounds(15,15,95,30);
         metkaDva.setBounds(15,115,200,30);
-
         fio.setBounds(120, 15, 100, 30);
-
         getContentPane().setLayout(null);
         getContentPane().add(button);
+        getContentPane().add(button1);
         getContentPane().add(metka);
         getContentPane().add(metkaDva);
         getContentPane().add(fio);
-
         setLocationRelativeTo(null);
     }
 }
+
--- Lab4KrutMJ/src/zapusk.java
+++ Lab4KrutMJ/src/zapusk.java
@@ -5,8 +5,7 @@ import javax.swing.*;
 
 public class zapusk {
     public static void main(String args[]){
-
-        okno mainFrame = new okno("Работа N4 Круть");
+        okno mainFrame = new okno("Работа №4 Круть");
         mainFrame.setSize(300, 200);
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setVisible(true);
