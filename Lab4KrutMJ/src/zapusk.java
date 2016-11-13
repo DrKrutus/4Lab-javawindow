c5c1d05743bcf4119696e8f3cb3415f1d9736999
DrKrutus <thekrutus@gmail.com>
2016-11-13 13:13:47 +0200

    newest

---
+++ Lab4KrutMJ/src/zapusk.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Maxim on 13.11.2016.
+ */
+import javax.swing.*;
+
+public class zapusk {
+    public static void main(String args[]){
+
+        okno mainFrame = new okno("Работа N4 Круть");
+        mainFrame.setSize(300, 200);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setVisible(true);
+    }
+}
